56ac8189bfd6f948baaed4c1a510392ef446ca9b
Loren Cipriano <loren25cipriano@gmail.com>
2021-06-22 11:36:24 -0700

    done

---
+++ index.js
@@ -0,0 +1,30 @@
+// objects
+// Scooter constructor
+//   ✓ should create a new scooter with three properties
+// Driver constructor
+//   ✓ should create a new driver with three properties
+// PickupLocation constructor
+//   ✓ should create a new PickupLocation with two properties
+
+
+function Scooter(year, color, model) {
+    this.year = year 
+    this.color = color 
+    this.model = model
+
+}
+
+
+
+function Driver(name, age, experience) {
+    this.name = name 
+    this.age = age 
+    this.experience = experience
+}
+
+
+
+function PickupLocation( address, city ) {
+    this.address = address 
+    this.city = city
+}
